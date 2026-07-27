af41b8d12ba4eed79047f253f4d7ca856b882517
ieejoobinin12 <majaporteleki683@gmail.com>
2026-07-27 13:02:26 +0200

    Update command prefix and card list formatting
    Changed command prefix from 'elidrop' to '!' and fixed card list formatting.

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True  # Required to read commands
 
-bot = commands.Bot(command_prefix="elidrop", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 # A list of your cards (you can add names, image links, or descriptions)
 cards_list = [
-    "Common Card: ano & eli",
+    "Common Card: ano & eli ",
     "Common Card: ano ",
     "Common Card: eli",
-    "Common Card: tung tung tung sahur"
+    "Common Card: tung tung tung sahurhttps ://cdn.discordapp.com/attachments/1450544431496429571/1531254222429884506/506410c5e062a21589fef3fd0bd6a575.webp.jpg?ex=6a688b1f&is=6a67399f&hm=9ace7896bb9e202778bf40fbf840249b60d202c3c16cd00b3bf60ed8a77b0f00&"
 ]
 
 @bot.event
