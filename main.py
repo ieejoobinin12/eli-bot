30221b1901195287bd0a84595b9e7d19ad13e88d
ieejoobinin12 <majaporteleki683@gmail.com>
2026-07-27 17:18:46 +0200

    Change command prefix from '!' to 'e'

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="e", intents=intents)
 
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 REPO_NAME = "ieejoobinin12/eli-bot"
