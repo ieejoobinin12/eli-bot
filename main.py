8c1a43a56a6cac4b51f5fd8211a4fe207de464b2
ieejoobinin12 <majaporteleki683@gmail.com>
2026-07-27 13:00:28 +0200

    Change command prefix and implement card drop command
    Updated the command prefix and added a card drop feature.

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
+import random
 import os
 import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
-intents.message_content = True
+intents.message_content = True  # Required to read commands
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="elidrop", intents=intents)
+
+# A list of your cards (you can add names, image links, or descriptions)
+cards_list = [
+    "Common Card: ano & eli",
+    "Common Card: ano ",
+    "Common Card: eli",
+    "Common Card: tung tung tung sahur"
+]
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}!")
 
 @bot.command()
-async def hello(ctx):
-    await ctx.send("Hello from Eli!")
+async def drop(ctx):
+    # Picks a random card from the list
+    chosen_card = random.choice(cards_list)
+    await ctx.send(f"🃏 A card has dropped! **{chosen_card}**")
 
-bot.run(os.getenv("DISCORD_TOKEN"))
+# Runs the bot using the token from Railway variables
+bot.run(os.getenv('DISCORD_TOKEN'))
+
